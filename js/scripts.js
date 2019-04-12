78280da8226928f8cd0cd75c9010fcb0d4123f3f
unknown <lugowski.k@gmail.com>
2019-04-12 20:31:37 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+// aside-menu activator
+
+const burger = document.querySelector('.burger');
+const activeElements = [...document.querySelectorAll('.active')];
+
+const displayAsideMenu = () => {
+    for (let key in activeElements) {
+        activeElements[key].classList.toggle('show')
+    }
+}
+
+burger.addEventListener('click', displayAsideMenu);
+
+//jumbotron animation
+
+let activeSlide = 0;
+const colorImg = document.querySelector('.color');
+const greyImg = document.querySelector('.grey');
+const h1 = document.querySelector('.member h1');
+const h2 = document.querySelector('.member h2');
+
+const colorImages = ['assets/welcome-slider/s1.png', 'assets/welcome-slider/s2.png', 'assets/welcome-slider/s3.png'];
+const greyImages = ['assets/welcome-slider/s1a.png', 'assets/welcome-slider/s2a.png', 'assets/welcome-slider/s3a.png'];
+const names = ['Anna Deadline', 'Mark Flexible', 'Joe TeamPlayer'];
+const professions = ['JS Programmer', 'UX UI designer', 'front-end developer'];
+
+const changeSlide = () => {
+    activeSlide++;
+
+    if (activeSlide === colorImages.length) {
+        activeSlide = 0;
+    }
+
+    colorImg.src = colorImages[activeSlide];
+    greyImg.src = greyImages[activeSlide];
+    h1.textContent = names[activeSlide];
+    h2.textContent = professions[activeSlide];
+}
+
+setInterval(changeSlide, 4000);
