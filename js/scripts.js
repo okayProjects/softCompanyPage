57d555b13835abb338de75d9034fa88adca15368
unknown <lugowski.k@gmail.com>
2019-04-16 14:17:18 +0200

    improve section 4, add js logic for opinion slider

--- js/scripts.js
+++ js/scripts.js
@@ -49,3 +49,55 @@ $('aside li a').on('click', function () {
         scrollTop: $(goToSection).offset().top
     });
 })
+
+
+//opinions
+
+const slideList = [
+    {
+        image: 'assets/opinions/portrait1.jpg',
+        opinion: 'Opinion no ONE: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis error magnam dolor numquam provident cupiditate laudantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis error magnam dolor numquam provident cupiditate laudantium',
+        jobTitle: 'Job Title One'
+    },
+    {
+        image: 'assets/opinions/portrait2.jpg',
+        opinion: 'Opinion no TWO: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis error magnam dolor numquam provident cupiditate laudantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis error magnam dolor numquam provident cupiditate laudantium',
+        jobTitle: 'Job Title TWO'
+    },
+    {
+        image: 'assets/opinions/portrait3.jpg',
+        opinion: 'Opinion no THREE: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis error magnam dolor numquam provident cupiditate laudantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis error magnam dolor numquam provident cupiditate laudantium',
+        jobTitle: 'Job Title THREE'
+    },
+    {
+        image: 'assets/opinions/portrait4.jpg',
+        opinion: 'Opinion no FOUR: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis error magnam dolor numquam provident cupiditate laudantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis error magnam dolor numquam provident cupiditate laudantium',
+        jobTitle: 'Job Title FOUR'
+    },
+    {
+        image: 'assets/opinions/portrait5.jpg',
+        opinion: 'Opinion no FIVE: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis error magnam dolor numquam provident cupiditate laudantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis error magnam dolor numquam provident cupiditate laudantium',
+        jobTitle: 'FIVE'
+    }
+]
+
+console.log(slideList);
+
+
+const image = document.querySelector('.s4-carusel-photo-wrapper img');
+const opinion = document.querySelector('.s4-carusel-wrapper blockquote i');
+const jobTitle = document.querySelector('.s4-carusel-wrapper h3');
+const time = 3000;
+let activeOpinionSlide = 0;
+
+const changeOpinionSlide = () => {
+    activeOpinionSlide++;
+    if (activeOpinionSlide === slideList.length) {
+        activeOpinionSlide = 0;
+    }
+    image.src = slideList[activeOpinionSlide].image;
+    opinion.textContent = slideList[activeOpinionSlide].opinion;
+    jobTitle.textContent = slideList[activeOpinionSlide].jobTitle;
+}
+
+setInterval(changeOpinionSlide, time);
